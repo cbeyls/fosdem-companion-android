4233549cbb8cec53197c8f7ea28aba6a8b4aac83
Abhishek Batra <abhishekbatra1993@gmail.com>
2015-03-02 23:41:42 +0530

    Code cleanup

--- app/src/main/java/org/fossasia/api/FosdemApi.java
+++ app/src/main/java/org/fossasia/api/FosdemApi.java
@@ -1,17 +1,5 @@
 package org.fossasia.api;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
-
-import org.fossasia.db.DatabaseManager;
-import org.fossasia.model.Event;
-import org.fossasia.parsers.EventsParser;
-import org.fossasia.utils.HttpUtils;
-
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 /**
  * Main API entry point.
  *
@@ -28,49 +16,4 @@ public class FosdemApi {
     public static final int RESULT_ERROR = -1;
     public static final int RESULT_UP_TO_DATE = -2;
 
-    private static final Lock scheduleLock = new ReentrantLock();
-
-    /**
-     * Download & store the schedule to the database. Only one thread at a time will perform the actual action, the other ones will return immediately. The
-     * result will be sent back in the form of a local broadcast with an ACTION_DOWNLOAD_SCHEDULE_RESULT action.
-     */
-    public static void downloadSchedule(Context context) {
-        if (!scheduleLock.tryLock()) {
-            // If a download is already in progress, return immediately
-            return;
-        }
-
-
-        int result = RESULT_ERROR;
-        try {
-            DatabaseManager dbManager = DatabaseManager.getInstance();
-            HttpUtils.HttpResult httpResult = HttpUtils.get(
-                    context,
-                    FosdemUrls.getSchedule(),
-                    dbManager.getLastModifiedTag(),
-                    ACTION_DOWNLOAD_SCHEDULE_PROGRESS,
-                    EXTRA_PROGRESS);
-            if (httpResult.inputStream == null) {
-                // Nothing to parse, the result is up-to-date.
-                result = RESULT_UP_TO_DATE;
-                return;
-            }
-
-            try {
-                Iterable<Event> events = new EventsParser().parse(httpResult.inputStream);
-                result = dbManager.storeSchedule(events, httpResult.lastModified);
-            } finally {
-                try {
-                    httpResult.inputStream.close();
-                } catch (Exception ignored) {
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ACTION_DOWNLOAD_SCHEDULE_RESULT).putExtra(EXTRA_RESULT, result));
-            scheduleLock.unlock();
-        }
-    }
 }
