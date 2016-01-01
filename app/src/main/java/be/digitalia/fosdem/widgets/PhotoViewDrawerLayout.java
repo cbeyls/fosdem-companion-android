719e387d9153dff24b773686d141b16819aa83f5
Christophe Beyls <software@digitalia.be>
2016-01-01 21:43:57 +0100

    Added a fix for crashes while zooming in PhotoView inside a DrawerLayout.

---
+++ app/src/main/java/be/digitalia/fosdem/widgets/PhotoViewDrawerLayout.java
@@ -0,0 +1,36 @@
+package be.digitalia.fosdem.widgets;
+
+import android.content.Context;
+import android.support.v4.widget.DrawerLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * DrawerLayout which includes a fix to prevent crashes with PhotoView.
+ * <p/>
+ * See https://github.com/chrisbanes/PhotoView#issues-with-viewgroups
+ * http://code.google.com/p/android/issues/detail?id=18990
+ */
+public class PhotoViewDrawerLayout extends DrawerLayout {
+
+	public PhotoViewDrawerLayout(Context context) {
+		super(context);
+	}
+
+	public PhotoViewDrawerLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public PhotoViewDrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		try {
+			return super.onInterceptTouchEvent(ev);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
