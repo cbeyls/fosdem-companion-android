2a4bedbccfcf0d3fb2e17a01ba2afba94e57ca6a
Christophe Beyls <software@digitalia.be>
2019-05-11 22:11:02 +0200

    Implement an AnimatedStateListDrawable for the bookmark status icon

--- app/src/main/java/be/digitalia/fosdem/widgets/BookmarkStatusAdapter.java
+++ app/src/main/java/be/digitalia/fosdem/widgets/BookmarkStatusAdapter.java
@@ -1,7 +1,5 @@
 package be.digitalia.fosdem.widgets;
 
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.Drawable;
 import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LifecycleOwner;
@@ -23,21 +21,14 @@ public class BookmarkStatusAdapter {
 		viewModel.getBookmarkStatus().observe(owner, bookmarkStatus -> {
 			if (bookmarkStatus == null) {
 				imageButton.setEnabled(false);
-				imageButton.setImageResource(R.drawable.ic_bookmark_outline_white_24dp);
+				imageButton.setSelected(false);
 			} else {
-				// Only animate updates, when the button was already enabled
-				final boolean animate = bookmarkStatus.isUpdate() && imageButton.isEnabled();
 				imageButton.setEnabled(true);
-				if (bookmarkStatus.isBookmarked()) {
-					imageButton.setContentDescription(imageButton.getContext().getString(R.string.remove_bookmark));
-					imageButton.setImageResource(animate ? R.drawable.avd_bookmark_add_24dp : R.drawable.ic_bookmark_white_24dp);
-				} else {
-					imageButton.setContentDescription(imageButton.getContext().getString(R.string.add_bookmark));
-					imageButton.setImageResource(animate ? R.drawable.avd_bookmark_remove_24dp : R.drawable.ic_bookmark_outline_white_24dp);
-				}
-				final Drawable drawable = imageButton.getDrawable();
-				if (drawable instanceof Animatable) {
-					((Animatable) drawable).start();
+				imageButton.setContentDescription(imageButton.getContext().getString(bookmarkStatus.isBookmarked() ? R.string.remove_bookmark : R.string.add_bookmark));
+				imageButton.setSelected(bookmarkStatus.isBookmarked());
+				// Only animate updates, when the button was already enabled
+				if (!bookmarkStatus.isUpdate() || !imageButton.isEnabled()) {
+					imageButton.jumpDrawablesToCurrentState();
 				}
 			}
 		});
