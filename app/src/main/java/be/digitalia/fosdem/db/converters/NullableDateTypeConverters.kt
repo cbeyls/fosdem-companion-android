7373a0b56697941d8d1aa076604a58c7a919e0a1
Christophe Beyls <software@digitalia.be>
2020-01-21 18:27:06 +0100

    convert Room TypeConverters to Kotlin

---
+++ app/src/main/java/be/digitalia/fosdem/db/converters/NullableDateTypeConverters.kt
@@ -0,0 +1,14 @@
+package be.digitalia.fosdem.db.converters
+
+import androidx.room.TypeConverter
+import java.util.*
+
+object NullableDateTypeConverters {
+    @JvmStatic
+    @TypeConverter
+    fun toDate(value: Long?): Date? = value?.let { Date(it) }
+
+    @JvmStatic
+    @TypeConverter
+    fun fromDate(value: Date?): Long? = value?.time
+}
