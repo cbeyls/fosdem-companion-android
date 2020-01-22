1cf39c3fa5937c8e357bb6fe656aa86f9f090d53
Christophe Beyls <software@digitalia.be>
2020-01-22 22:07:50 +0100

    fix incorrect resource annotation in RoomStatus

--- app/src/main/java/be/digitalia/fosdem/model/RoomStatus.kt
+++ app/src/main/java/be/digitalia/fosdem/model/RoomStatus.kt
@@ -5,7 +5,7 @@ import androidx.annotation.StringRes
 import be.digitalia.fosdem.R
 
 enum class RoomStatus(@StringRes @get:StringRes val nameResId: Int,
-                      @ColorRes @get:StringRes val colorResId: Int) {
+                      @ColorRes @get:ColorRes val colorResId: Int) {
     OPEN(R.string.room_status_open, R.color.room_status_open),
     FULL(R.string.room_status_full, R.color.room_status_full),
     EMERGENCY_EVACUATION(R.string.room_status_emergency_evacuation, R.color.room_status_emergency_evacuation)
