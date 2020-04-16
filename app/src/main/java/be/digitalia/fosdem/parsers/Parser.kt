75c73c2de832ec367326ad69e81d55341964f8e7
Christophe Beyls <software@digitalia.be>
2020-04-16 16:15:56 +0200

    update Parser to be a read-only interface

--- app/src/main/java/be/digitalia/fosdem/parsers/Parser.kt
+++ app/src/main/java/be/digitalia/fosdem/parsers/Parser.kt
@@ -2,7 +2,7 @@ package be.digitalia.fosdem.parsers
 
 import okio.BufferedSource
 
-interface Parser<T> {
+interface Parser<out T> {
     @Throws(Exception::class)
     fun parse(source: BufferedSource): T
 }
