e306f6436be7e0ce1ea7036c8067a0f9590dfe16
Christophe Beyls <software@digitalia.be>
2020-09-14 14:41:50 +0200

    revert workaround for compound drawables tinting bug in some Android versions, bug has been fixed

--- app/src/main/java/be/digitalia/fosdem/adapters/EventsAdapter.kt
+++ app/src/main/java/be/digitalia/fosdem/adapters/EventsAdapter.kt
@@ -100,7 +100,7 @@ class EventsAdapter constructor(context: Context, owner: LifecycleOwner, private
             this.event = event
 
             title.text = event.title
-            val bookmarkDrawable = if (isBookmarked) AppCompatResources.getDrawable(context, R.drawable.ic_bookmark_24dp) else null
+            val bookmarkDrawable = if (isBookmarked) AppCompatResources.getDrawable(context, R.drawable.ic_bookmark_white_24dp) else null
             TextViewCompat.setCompoundDrawablesRelativeWithIntrinsicBounds(title, null, null, bookmarkDrawable, null)
             title.contentDescription = if (isBookmarked) {
                 context.getString(R.string.in_bookmarks_content_description, event.title ?: "")
