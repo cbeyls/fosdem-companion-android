08a1e2235c9af00d60a5e965ae719fad9ed1e4a3
Christophe Beyls <software@digitalia.be>
2024-01-06 01:22:41 +0100

    feat: add migration from db v3 (in production) to v5

--- app/src/main/java/be/digitalia/fosdem/inject/DatabaseModule.kt
+++ app/src/main/java/be/digitalia/fosdem/inject/DatabaseModule.kt
@@ -9,10 +9,13 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import be.digitalia.fosdem.db.AppDatabase
 import be.digitalia.fosdem.db.BookmarksDao
 import be.digitalia.fosdem.db.ScheduleDao
+import be.digitalia.fosdem.model.Attachment
+import be.digitalia.fosdem.model.Day
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -42,8 +45,32 @@ object DatabaseModule {
         @ApplicationContext context: Context,
         @Named("Database") dataStore: DataStore<Preferences>
     ): AppDatabase {
+        val migration3to5 = Migration(3, 5) { db ->
+            with(db) {
+                // Clear schedule (but keep bookmarks)
+                execSQL("DELETE FROM events")
+                execSQL("DELETE FROM events_titles")
+                execSQL("DELETE FROM persons")
+                execSQL("DELETE FROM events_persons")
+                execSQL("DELETE FROM links")
+                execSQL("DELETE FROM tracks")
+
+                // Create table attachments
+                execSQL("CREATE TABLE IF NOT EXISTS ${Attachment.TABLE_NAME} (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `event_id` INTEGER NOT NULL, `url` TEXT NOT NULL, `description` TEXT)")
+                execSQL("CREATE INDEX IF NOT EXISTS `attachment_event_id_idx` ON ${Attachment.TABLE_NAME} (`event_id`)")
+
+                // Recreate table days with new columns
+                execSQL("DROP TABLE IF EXISTS ${Day.TABLE_NAME}")
+                execSQL("CREATE TABLE IF NOT EXISTS ${Day.TABLE_NAME} (`index` INTEGER NOT NULL, `date` INTEGER NOT NULL, `start_time` INTEGER NOT NULL, `end_time` INTEGER NOT NULL, PRIMARY KEY(`index`))")
+            }
+            runBlocking {
+                dataStore.edit { it.clear() }
+            }
+        }
+
         return Room.databaseBuilder(context, AppDatabase::class.java, DB_FILE)
             .setJournalMode(RoomDatabase.JournalMode.TRUNCATE)
+            .addMigrations(migration3to5)
             .fallbackToDestructiveMigration()
             .addCallback(object : RoomDatabase.Callback() {
                 @WorkerThread
