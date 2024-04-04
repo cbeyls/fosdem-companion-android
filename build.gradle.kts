62bbd61694f245a997102679393a1532fc0608a0
Christophe Beyls <software@digitalia.be>
2024-04-04 18:58:29 +0200

    chore: migrate to version catalog TOML file and upgrade AGP to 8.3.1, Kotlin to 1.9.23, KSP to 1.0.19, Coroutines to 1.8.0, Hilt to 2.51.1, Okio to 3.9.0, Moshi to 1.15.1, Browser to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    id("com.google.dagger.hilt.android") apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    val hiltVersion: String by settings
-    plugins {
-        id("com.google.dagger.hilt.android") version hiltVersion
-    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -20,22 +16,6 @@ dependencyResolutionManagement {
         }
         google()
         mavenCentral()
-    }
-
-    versionCatalogs {
-        val hiltVersion: String by settings
-        create("libs") {
-            version("hilt", hiltVersion)
-            version("room", "2.6.1")
-            version("lifecycle", "2.7.0")
-            library("hilt-android", "com.google.dagger", "hilt-android").versionRef("hilt")
-            library("hilt-compiler", "com.google.dagger", "hilt-compiler").versionRef("hilt")
-            library("room-runtime", "androidx.room", "room-ktx").versionRef("room")
-            library("room-paging", "androidx.room", "room-paging").versionRef("room")
-            library("room-compiler", "androidx.room", "room-compiler").versionRef("room")
-            library("lifecycle-runtime", "androidx.lifecycle", "lifecycle-runtime-ktx").versionRef("lifecycle")
-            library("lifecycle-viewmodel", "androidx.lifecycle", "lifecycle-viewmodel-ktx").versionRef("lifecycle")
-        }
     }
 }
 
